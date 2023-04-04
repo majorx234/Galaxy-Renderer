f31174c9427b93a7f1f1708a4336926480c4f2f7
nikl174 <niclasst81@gmail.com>
2023-04-04 02:28:30 +0200

    added a simple cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,57 @@
 #include <iostream>
+#include <string>
 
 #include "GalaxyWnd.hpp"
 
-int main(int argc, char** argv)
-{
-	try
-	{
-		GalaxyWnd wndMain;
-		wndMain.InitFullscreen(35000.0, "Rendering a Galaxy with Density Waves");
-		wndMain.MainLoop();
-	}
-	catch (std::exception& exc)
-	{
-		std::cout << "Fatal error: " << exc.what() << std::endl;
-	}
-	catch (...)
-	{
-		std::cout << "Fatal error: unexpected exception" << std::endl;
-	}
+/* TODO: delete inimplemented sign if finished <04-04-23, nikl> */
+std::string helpstr() {
+  return "default:	open in gtk window (CURRENTLY UNIMPLEMENTED!)\n"
+         "-f		open in Fullscreen mode\n"
+         "-l		open in legacy SDL-Window\n";
+}
 
-	return 0;
+std::string unknownCmd(const char *original_cmd) {
+  return "Unknown Argumand '" + std::string(original_cmd) +
+         "', type -h for help";
+}
+
+int main(int argc, char **argv) {
+  try {
+    if (argv[1]) {
+      const char *arg = argv[1];
+      switch (arg[0]) {
+      case '-':
+        switch (arg[1]) {
+        case 'f': {
+          GalaxyWnd wndMain;
+          wndMain.InitFullscreen(35000.0,
+                                 "Rendering a Galaxy with Density Waves");
+          wndMain.MainLoop();
+        } break;
+        case 'l': {
+          GalaxyWnd wndMain;
+          wndMain.Init(1600, 1000, 35000.0,
+                       "Rendering a Galaxy with Density Waves");
+          wndMain.MainLoop();
+          break;
+        }
+        case 'h':
+          std::cout << helpstr() << std::endl;
+          break;
+        default:
+          std::cout << unknownCmd(arg) << std::endl;
+        }
+        break;
+      default:
+        std::cout << helpstr() << std::endl;
+      }
+    } else
+      std::cout << helpstr() << std::endl;
+  } catch (std::exception &exc) {
+    std::cout << "Fatal error: " << exc.what() << std::endl;
+  } catch (...) {
+    std::cout << "Fatal error: unexpected exception" << std::endl;
+  }
+
+  return 0;
 }
